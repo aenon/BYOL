4a3a5ad09e80c36a4be9298cee0f46a62f791f72
Xilin Sun <s.sn.giraffe@gmail.com>
2017-10-27 22:49:59 -0700

    use readline instead of fget

--- lispy.c
+++ lispy.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <editline/readline.h>
+#include <editline/history.h>
 
 static char input[2048];
 
 int main(int argc, char** argv) {
+  /* Print version and some help */
   puts("Lispy Version 0.0.1");
   puts("Press Ctrl-C to Exit\n");
 
   while (1) {
-    fputs("lispy> ", stdout);
-    fgets(input, 2048, stdin);
+    /* Output the prompt and get input */
+    char* input = readline("lispy>> ");
+
+    /* Echo input back to user */
     printf("You said %s\n", input);
+    free(input)
   }
 
   return 0;

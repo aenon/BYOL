87c65d35ca91c0abe51d2696725aa1331b0444c4
Xilin Sun <s.sn.giraffe@gmail.com>
2017-10-28 21:22:47 -0700

    evaluation

--- infix.c
+++ infix.c
@@ -24,6 +24,34 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
+/* Check which operator is used */
+long eval_op(char* op, long x, long y) {
+  if (strcmp(op, "+") == 0) { return x + y;}
+  if (strcmp(op, "-") == 0) { return x - y;}
+  if (strcmp(op, "*") == 0) { return x * y;}
+  if (strcmp(op, "/") == 0) { return x / y;}
+  return 0;
+}
+
+long eval(mpc_ast_t* t) {
+  /* Return a number directly */
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  char* op = t->children[1]->contents;
+
+  long x = eval(t->children[2]);
+
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(op, x, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
+
 int main(int argc, char** argv) {
 
   /* Create some parsers */
@@ -44,7 +72,7 @@ int main(int argc, char** argv) {
     Number, Operator, Expr, Lispy);
 
   /* Print version and some help */
-  puts("Lispy Version 0.0.0.0.2i");
+  puts("Lispy Version 0.0.0.0.3i");
   puts("Press Ctrl-C to Exit\n");
 
   while (1) {
@@ -57,7 +85,9 @@ int main(int argc, char** argv) {
     /* Attempt to parse the user input */
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
-      mpc_ast_print(r.output);
+
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
     } else {
       mpc_err_print(r.error);
